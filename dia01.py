96336f7fbd734ddcd3c46d0a35cb6ce4bcc58aa0
HerculesDEV7 <herculessantos891@gmail.com>
2026-02-27 13:04:56 -0300

    Dia 01 - variaveis, f-strings e input

---
+++ dia01.py
@@ -0,0 +1,18 @@
+# Minhas primeiras variáveis
+nome = "Hercules"
+idade = 28
+altura = 1.73
+estudando_python = True
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}m")
+print(f"Estudando Python: {estudando_python}")
+print(f"Olá, {nome}! Você tem {idade} anos e está indo muito bem!")
+
+# Entrada de dados
+nome = input("Qual é seu nome? ")
+idade = input("Qual é sua idade? ")
+
+print(f"Olá, {nome}! Você tem {idade} anos.")
+print(f"Em 10 anos você terá {int(idade) + 10} anos.")
